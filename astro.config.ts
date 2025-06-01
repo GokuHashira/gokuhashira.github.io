efb94b196a7f3afee88ee69a96525b1d48c1f5aa
GokuHashira <gokulsenpai03@gmail.com>
2025-06-02 00:25:47 +0530

    config change

--- astro.config.ts
+++ astro.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI ? 'https://gokuhashira.github.io' : 'http://localhost:4321',
-  // base: process.env.CI ? '/gokul-astro-project' : '/',
-  base: '/gokul-astro-project',
+  site: 'https://gokuhashira.github.io',
+  base: process.env.CI ? '/gokul-astro-project' : '/',  // Use base only in production
   integrations: [react(), tailwind({
     applyBaseStyles: false
   })]
